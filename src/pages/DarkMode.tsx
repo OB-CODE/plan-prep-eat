8cc223882614f9751cbc887f23aff6852e1dd898
mitchell <mitchell.s.obrien@gmail.com>
2023-03-23 09:02:44 +1100

    dark mode

---
+++ src/pages/DarkMode.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+const toggleDark = (event: React.MouseEvent<HTMLButtonElement>) => {
+  event.preventDefault();
+
+  const button: HTMLButtonElement = event.currentTarget;
+  document.documentElement.classList.toggle('dark')
+}
+
+function DarkMode() {
+  return (
+    <div>
+
+      <button onClick={toggleDark} className='absolute left-5 top-5 bg-purple-500 p-1 text-1xl font-extrabold tracking-tight toggle-dark'
+      
+      >Dark Mode</button>
+    </div>
+  )
+}
+
+export default DarkMode
