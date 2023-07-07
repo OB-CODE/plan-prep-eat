204ec4b55dd970933471763f0d0b1c84f69dd962
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2023-07-07 17:54:32 +1000

    dark mode changes

--- src/pages/DarkMode.tsx
+++ src/pages/DarkMode.tsx
@@ -1,36 +1,70 @@
-import React, { useState } from 'react';
-import { Button, buttonVariants } from '~/UI/button';
+import React, { useState, useEffect } from "react";
+import { Button, buttonVariants } from "~/UI/button";
+import { useRouter } from 'next/router';
 
+// const router = useRouter();
 
-const toggleDark = (event: React.MouseEvent<HTMLButtonElement>) => {
-  event.preventDefault();
+// useEffect(() => {
+//   // Add 'dark' class to documentElement
+//   document.documentElement.classList.add('dark');
+// },
+//  [router.pathname]);
 
-
-  const button: HTMLButtonElement = event.currentTarget;
-  document.documentElement.classList.toggle('dark')
-  console.log(document.documentElement.classList.value.includes('dark'))
-}
-
-function DarkMode() {
-  const [inDarkMode, setInDarkMode ] = useState(true)
-
-  // const toggleDark = () => {
-  //   setInDarkMode(!inDarkMode);
-  //   document.documentElement.classList.toggle('dark')
-  // };
-
-  // add dark mode on launching app.
-  if (typeof document !== 'undefined') {
-    document.documentElement.classList.add('dark');
+function ModeToggle() {
+  function disableTransitionsTemporarily() {
+    document.documentElement.classList.add('[&_*]:!transition-none')
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('[&_*]:!transition-none')
+    }, 0)
   }
-    return (
-    <div className='bg-slate-300 dark:bg-slate-600 w-full fixed h-16 top-0 sm:h-24'>
-      <Button onClick={toggleDark}  className={buttonVariants({ className: 'relative left-2 top-2 sm:left-5 sm:top-5', variant: "purple", size: "lg"})} children={'Dark Mode'}/>
 
-      {/* <button onClick={toggleDark} className='absolute left-5 top-5 bg-purple-500 p-1 text-1xl font-extrabold tracking-tight toggle-dark' */}
-      {/* >Dark Mode</button> */}
-    </div>
+  function setMode(mode) {
+    disableTransitionsTemporarily();
+    document.documentElement.classList.toggle('dark', mode === 'dark');
+    window.localStorage.isDarkMode = (mode === 'dark');
+  }
+
+  // Get the initial mode based on user preference
+  // const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  // const isSystemDarkMode = darkModeMediaQuery.matches;
+  // const savedMode = window.localStorage.isDarkMode;
+
+  // Set the initial mode based on user preference or saved mode
+  // if (savedMode !== undefined) {
+  //   setMode(savedMode === 'true' ? 'dark' : 'light');
+  // } else {
+  //   setMode(isSystemDarkMode ? 'dark' : 'light');
+  // }
+
+  function toggleMode() {
+    const currentMode = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+    const newMode = currentMode === 'dark' ? 'light' : 'dark';
+    setMode(newMode);
+  }
+
+  return (
+    <Button
+    aria-label="Toggle dark mode"
+        onClick={toggleMode}
+        className={buttonVariants({
+          className: "relative left-2 top-2 sm:left-5 sm:top-5",
+          variant: "purple",
+          size: "lg",
+        })}
+        children={"Dark Mode"}
+      />
   )
 }
 
-export default DarkMode
+function DarkMode() {
+    // // Set the initial state of the app to start in dark mode
+    // document.documentElement.classList.add('dark');
+
+  return (
+    <div className="fixed top-0 h-16 w-full bg-purple-300 dark:bg-purple-900 sm:h-24">
+      <ModeToggle />
+    </div>
+  );
+}
+
+export default DarkMode;
--- src/pages/PantryStaples.tsx
+++ src/pages/PantryStaples.tsx
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
-import { defaultPantry } from '../store/selectedFoodStore'
+import { defaultPantry } from "../store/selectedFoodStore";
 
 const PantryStaples = () => {
-
   const pantry = defaultPantry((state) => state.pantry);
   const setPantry = defaultPantry((state) => state.setPantry);
-  const handleCheckPantry = defaultPantry((state) => state.handleCheckPantry)
+  const handleCheckPantry = defaultPantry((state) => state.handleCheckPantry);
 
   return (
     <div>
-      <div className="list-container flex flex-col gap-1 justify-center pb-3">
-          {pantry.map((item, index) => (
-            <div key={index}>
-              <input
+      <div className="list-container flex flex-col justify-center gap-1 pb-3">
+        {pantry.map((item, index) => (
+          <div key={index}>
+            <input
               name={item.item}
               value={item.checked}
               checked={item.checked}
               type="checkbox"
               onChange={handleCheckPantry}
-              />
+            />
             <span>{item.item}</span>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PantryStaples
+export default PantryStaples;
