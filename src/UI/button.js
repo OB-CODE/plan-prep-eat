6fc573164725ca58ce865bdd49e0a18469cc946d
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2023-07-07 20:59:41 +1000

    pre rebase

--- src/UI/button.js
+++ src/UI/button.js
@@ -5,17 +5,17 @@ import { cva } from "class-variance-authority";
 import { concatenateClassnames } from "./utilities";
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center rounded-full text-sm font-medium tracking-wide uppercase transition-all duration-300 ease-in-out focus:ring-offset-2 focus:ring-slate-400 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none',
-    {
+  "inline-flex items-center justify-center rounded-full text-sm font-medium tracking-wide uppercase transition-all duration-300 ease-in-out focus:ring-offset-2 focus:ring-slate-400 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none",
+  {
     variants: {
       variant: {
         default:
           "bg-gray-400 text-white hover:bg-gray-500 dark:bg-slate-50 dark:text-slate-900",
 
-          purple:
-          'bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full dark:bg-purple-400 dark:hover:bg-purple-600 dark:text-white',
-          lightB:
-          'mb-2 w-fit border-2 border-blue-300 bg-blue-100 p-1 text-blue-600 hover:bg-blue-200',
+        purple:
+          "bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full dark:bg-purple-400 dark:hover:bg-purple-600 dark:text-white",
+        lightB:
+          "mb-2 w-fit border-2 border-blue-300 bg-blue-100 p-1 text-blue-600 hover:bg-blue-200",
 
         link: "bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-300 hover:bg-transparent dark:hover:bg-transparent",
       },
@@ -36,7 +36,9 @@ const Button = React.forwardRef(
   ({ className, children, href, variant, size, ...props }, ref) => {
     return (
       <button
-        className={concatenateClassnames(buttonVariants({ variant, size, className }))}
+        className={concatenateClassnames(
+          buttonVariants({ variant, size, className })
+        )}
         ref={ref}
         {...props}
       >
--- src/UI/utilities.js
+++ src/UI/utilities.js
@@ -1,9 +1,8 @@
 // import { ClassValue, clsx } from 'clsx'
-import { clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 // export function cn(...inputs: ClassValue[]) {
-    export function concatenateClassnames(...inputs) {
-
-    return twMerge(clsx(inputs))
-  }
+export function concatenateClassnames(...inputs) {
+  return twMerge(clsx(inputs));
+}
