306c1016a0004a636d9580259333eb747e2e7611
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2023-07-16 19:41:40 +1000

    first calculator added

---
+++ src/pages/MacroCalculators/ManualButtonCalculator.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { Button, buttonVariants } from '~/UI/button';
+
+const ManualButtonCalculator = () => {
+        // For counting calories:
+        const [protein, setProtein] = useState('');
+        const [carbohydrates, setCarbohydrates] = useState('');
+        const [fat, setFat] = useState('');
+        const [calories, setCalories] = useState(0);
+      
+        const calculateCalories = () => {
+          const proteinCalories = parseInt(protein) * 4;
+          const carbohydratesCalories = parseInt(carbohydrates) * 4;
+          const fatCalories = parseInt(fat) * 9;
+          const totalCalories = proteinCalories + carbohydratesCalories + fatCalories;
+          setCalories(totalCalories);
+        };
+
+        // CSS for the input tags
+        const inputStyle = {
+            width: '50px',
+            textAlign: 'center',
+            backgroundColor: 'lightgray',
+            color: 'black',
+          };
+
+          const inputs = [
+            { id: 'protein', label: 'Protein (g)', value: protein, setter: setProtein },
+            { id: 'carbohydrates', label: 'Carbohydrates (g)', value: carbohydrates, setter: setCarbohydrates },
+            { id: 'fat', label: 'Fat (g)', value: fat, setter: setFat },
+          ];
+
+  return (
+    <div>
+
+    <div className='text-xl font-bold	'>Calorie Calculator</div>
+    <h1>Calculated by react in the front-end. Click the button to calculate. </h1>
+
+<div className='MacroContainer py-3 flex flex-col gap-1 w-1/4 mx-auto'>
+<div>
+    {inputs.map((input) => (
+      <div key={input.id} className='flex justify-between'>
+        <label htmlFor={input.id}>{input.label}</label>
+        <input
+          id={input.id}
+          type="number"
+          value={input.value}
+          style={inputStyle}
+          onChange={(e) => input.setter(e.target.value)}
+        />
+      </div>
+    ))}
+    </div>
+</div>
+
+    <Button onClick={calculateCalories} className={buttonVariants({
+          variant: "purple",
+          size: "lg",
+        })}
+        children={"Calculate Calories"} ></Button>
+    <div className='pt-3 flex gap-4 mx-0-auto justify-center'>
+      <label htmlFor="calories">Total Calories:</label>
+      <input
+        id="calories"
+        type="number"
+        value={calories}
+        style={inputStyle}
+
+        disabled
+      />
+    </div>
+  </div>  )
+}
+
+export default ManualButtonCalculator
--- src/pages/MacroPage.tsx
+++ src/pages/MacroPage.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ManualButtonCalculator from './MacroCalculators/ManualButtonCalculator';
+
 
 const MacroPage = () => {
+
   return (
     <div className='text-center'>
             <h1 className="text-5xl pb-5 font-extrabold tracking-tight dark:text-white sm:text-[5rem]">
               Plan <span className="text-[hsl(280,100%,70%)]">PREP</span> Eat
             </h1>
-    <div>MacroPage</div>
+    <ManualButtonCalculator />
     </div>
 
 
