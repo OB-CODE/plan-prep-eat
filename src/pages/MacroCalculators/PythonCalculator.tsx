e7510c2702153a829687fe3bf378ff501af44c49
tinanvo <tinanhivo@gmail.com>
2023-07-27 10:57:51 +1000

    Created AJAX request, successfully sent POST request to Django API

--- src/pages/MacroCalculators/PythonCalculator.tsx
+++ src/pages/MacroCalculators/PythonCalculator.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Button, buttonVariants } from "~/UI/button";
 
 const PythonCalculator = () => {
-  return (
-    <div>PythonCalculator</div>
-  )
-}
+  const sendDataToDjango = async () => {
+    const dataToSend = { key: "HELLO MITCH" }; // Data you want to send
+   
+    try {
+      const response = await fetch("http://localhost:8000/api/process_data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataToSend),
+      });
 
-export default PythonCalculator
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const responseData = await response.json(); // Processed data from Django
+      // Handle the response data from Django as needed
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      PythonCalculator
+      <Button onClick={sendDataToDjango} type="submit" className={buttonVariants({
+          variant: "purple",
+          size: "lg",
+        })}
+        children={"Calculate Calories"}>
+      </Button>
+    </div>
+  );
+};
+
+export default PythonCalculator;
