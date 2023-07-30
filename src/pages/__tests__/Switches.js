07c3c36ed61e64c36a0d2930909d22da52519a2b
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2023-07-30 19:30:24 +1000

    test removed for CI

---
+++ src/pages/__tests__/Switches.js
@@ -0,0 +1,11 @@
+// import * as React from 'react'
+
+// import '@testing-library/jest-dom'
+// import {render} from '@testing-library/react'
+
+
+// import Switches from '../Switches'
+
+// test('Checkes the component renders', () => {
+//     render(<h1></h1>)
+// })
